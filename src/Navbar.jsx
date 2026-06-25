352132ef40965519d8b8922306abeb3fdfc83ad1
Mehak <mehakaroraaa111@gmail.com>
2026-06-25 10:32:55 +0530

    6th commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,28 @@
+import "./Navbar.css"
+import Logo from "./assets/pms-imgs/pms-logo.png"
 function Navbar(){
     return ( 
         <div>
-<h1>Hi my name is mehak</h1>
+<div>
+<nav className="Nav_bar">
+    <h2 className="logo">Logo</h2>
+   <img src={Logo}></img>
+
+    <a href="#">home</a>
+    <a href="#">About</a>
+    <a href="#">Section</a>
+    <a href="#">contact</a>
+    <a href="#">Courses</a>
+<div className="Sign_up">
+    
+        <a href="#">Login</a>
+        <a href="#">Sign-up</a>
+
+</div>
+
+</nav>
+
+</div>
         </div>
     )
 }
