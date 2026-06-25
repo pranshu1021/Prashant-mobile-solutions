bb9ffe2b70c405a152cf6c114c3a96d9c5e8902c
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-06-25 10:46:09 +0530

    7th commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,29 +2,31 @@ import "./Navbar.css"
 import Logo from "./assets/pms-imgs/pms-logo.png"
 function Navbar(){
     return ( 
-        <div>
-<div>
-<nav className="Nav_bar">
-    <h2 className="logo">Logo</h2>
-   <img src={Logo}></img>
-
-    <a href="#">home</a>
-    <a href="#">About</a>
-    <a href="#">Section</a>
-    <a href="#">contact</a>
-    <a href="#">Courses</a>
-<div className="Sign_up">
+        <header className = "navbar">
+        <div className="nav-container">
     
-        <a href="#">Login</a>
-        <a href="#">Sign-up</a>
+    <div className="brand">
+    <img src={Logo} alt="Prashant Mobile Solutions Logo" className="logo"/>
+    <h2>Prashant Mobile Solutions</h2>
+    </div>
 
-</div>
-
+<nav className="nav_links">
+    <a href="#">home</a>
+    <a href="#services">Services</a>
+    <a href="#about">About</a>
+    <a href="#">Courses</a>
+    <a href="#">Contact</a>
 </nav>
 
+
+<div className="nav-buttons">
+        <a className="login-btn" href="#login">Login</a>
+        <a className="signup-btn" href="#login">Sign Up</a>
 </div>
-        </div>
+</div>
+</header>
     )
 }
+
 
 export default Navbar;
