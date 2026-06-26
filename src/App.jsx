54f736e0fa3a278fee87f67af7b6e50aaeb9f6ea
pradeep-tanwar166 <pradeeptanwar166@gmail.com>
2026-06-26 13:29:52 +0530

    services card made

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero.jsx'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import Services from './components/Services.jsx';
 
 function App(){
     return( 
@@ -8,6 +9,7 @@ function App(){
        
        <Navbar/> 
        <Hero/>
+       <Services/>
     </div>
     )
 }
