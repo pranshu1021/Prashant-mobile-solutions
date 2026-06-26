a6eaf107d69ccaebb1f3ea03ca1253239cc9d7f9
Mehak <mehakaroraaa111@gmail.com>
2026-06-26 13:31:25 +0530

    15th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero.jsx'
 import Navbar from './components/Navbar.jsx'
+import Service from './components/Service.jsx'
 
 function App(){
     return( 
@@ -8,6 +9,7 @@ function App(){
        
        <Navbar/> 
        <Hero/>
+       <Service/>
     </div>
     )
 }
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,7 +1,19 @@
+import './Service.css'
+import { SiAuth0 } from "react-icons/si";
 
 function Service(){
     return (
-<div></div>
+<div className="card">
+
+<div className="image">
+  <SiAuth0 size={35}/>
+
+</div>
+<div className="content">
+<h4>Certified Technicians</h4>
+<p>Industry-trained & Verified experts</p>
+</div>
+</div>
     )
 }
 
