f708e9246a8e41a3622089c532ee47f3bf5eb067
pradeep-tanwar166 <pradeeptanwar166@gmail.com>
2026-06-27 13:19:29 +0530

    22th commit

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,124 +1,96 @@
-import "./Course.css"
+import "./Course.css";
 import { GiVintageRobot } from "react-icons/gi";
 import { GiAutoRepair } from "react-icons/gi";
 import { FaApple } from "react-icons/fa";
 
+function Course() {
+  return (
+    <div>
 
-function Course (){
-    return ( <div>
-        <div className="Mobile-card">
+        <div className="cards">
+      <div className="Mobile-card">
         <div className="image">
-        <GiVintageRobot />
-
-        
-        
+          <GiVintageRobot />
         </div>
 
         <div className="content">
-        <h2>Android Mobile Repair-Basic</h2>
-        <p>Learn Android device basics,hardware components,disassembly,soldering basics and 
-            common fault repairs from scratch.
-        </p>
-        <div className="Content-description">
-        <strong>4 weeks</strong>
-        <span>-Full time</span>
-</div>
-<div className="Content-desc-eligibility">
-<span>Eligible:</span>
-<strong>10th Pass</strong>
-
-</div>
-<div className="Content-desc-condition">
-    <span>Certificate included</span>
-
-
-
-</div>
-
+          <h2>Android Mobile Repair-Basic</h2>
+          <p>
+            Learn Android device basics,hardware
+            components,disassembly,soldering basics and common fault repairs
+            from scratch.
+          </p>
+          <div className="Content-description">
+            <strong>4 weeks</strong>
+            <span>-Full time</span>
+          </div>
+          <div className="Content-desc-eligibility">
+            <span>Eligible:</span>
+            <strong>10th Pass</strong>
+          </div>
+          <div className="Content-desc-condition">
+            <span>Certificate included</span>
+          </div>
         </div>
-<div className="enroll-butn">Enroll Now</div>
+        <div className="enroll-butn">Enroll Now</div>
+      </div>
 
 
-
-        </div>
-
-
-<div className="Mobile-card">
+      <div className="Mobile-card">
         <div className="image">
-        <GiAutoRepair />
-
-
-        
-        
+          <GiAutoRepair />
         </div>
-        
+
         <div className="content">
-        <h2>Advanced Android Repair</h2>
-        <p> Advanced chip-level repairs,eMMC/CPU programming,board tracking,software issues
-            and advanced troubleshooting techniques.
-        </p>
-        <div className="Content-description">
-        <strong>8 Weeks</strong>
-        <span>-Full-time</span>
-</div>
-<div className="Content-desc-eligibility">
-<span>Eligible:</span>
-<strong>12th Pass</strong>
-
-</div>
-<div className="Content-desc-condition">
-    <span>Certificate included</span>
-
-
-
-</div>
-
+          <h2>Advanced Android Repair</h2>
+          <p>
+            {" "}
+            Advanced chip-level repairs,eMMC/CPU programming,board
+            tracking,software issues and advanced troubleshooting techniques.
+          </p>
+          <div className="Content-description">
+            <strong>8 Weeks</strong>
+            <span>-Full-time</span>
+          </div>
+          <div className="Content-desc-eligibility">
+            <span>Eligible:</span>
+            <strong>12th Pass</strong>
+          </div>
+          <div className="Content-desc-condition">
+            <span>Certificate included</span>
+          </div>
         </div>
-<div className="enroll-butn">Enroll Now</div>
+        <div className="enroll-butn">Enroll Now</div>
+      </div>
 
 
-
-        </div>
-
-
-        <div className="Mobile-card">
+      <div className="Mobile-card">
         <div className="image">
-       <FaApple />
-
-
-        
-        
+          <FaApple />
         </div>
-        
+
         <div className="content">
-        <h2>Complete iPhone Repair</h2>
-        <p>Complete training on iPhone hardware,Face ID , True Tone,baseband issues,
-            microsoldering,and advanced chip-level repairs.
-        </p>
-        <div className="Content-description">
-        <strong>10 weeks</strong>
-        <span>-Full time</span>
-</div>
-<div className="Content-desc-eligibility">
-<span>Eligible:</span>
-<strong>12th Pass</strong>
-
-</div>
-<div className="Content-desc-condition">
-    <span>Certificate included</span>
-
-
-
-</div>
-
+          <h2>Complete iPhone Repair</h2>
+          <p>
+            Complete training on iPhone hardware,Face ID , True Tone,baseband
+            issues, microsoldering,and advanced chip-level repairs.
+          </p>
+          <div className="Content-description">
+            <strong>10 weeks</strong>
+            <span>-Full time</span>
+          </div>
+          <div className="Content-desc-eligibility">
+            <span>Eligible:</span>
+            <strong>12th Pass</strong>
+          </div>
+          <div className="Content-desc-condition">
+            <span>Certificate included</span>
+          </div>
         </div>
-<div className="enroll-butn">Enroll Now</div>
-
-
-
-        </div>
-
-        </div>
-    )
+        <div className="enroll-butn">Enroll Now</div>
+      </div>
+      </div>
+    </div>
+  );
 }
 export default Course;
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,35 +1,38 @@
-import "./Courses.css"
+import "./Courses.css";
 function Courses() {
   return (
     <div>
-        <div className="card">
-           <img src="https://thenounproject.com/icon/book-1183858/" alt="" /> 
-           <span>Begineer</span>
+      <div className="card">
+        <img src="https://thenounproject.com/icon/book-1183858/" alt="" />
+        <span>Begineer</span>
 
-<div className="Service-content">
-<h2>Foundation in Mobile Repair</h2>
-<p>Learn Smartphone hardware fundamentals,repair tools, and core techniques from the groud  up.</p>
-</div>
-<div className="course-details">
-<div className="detail">
-<span>
-<div>Duration</div>
-<div>Eligibilty</div>
-<div>Training</div>
-<div>Certificate</div>
-</span>
-<strong>
-<div>4 weeks</div>
-<div>10th pass</div>
-<div>Practical</div>
-<div>Included</div>
-</strong>
-</div>
-</div>
- <div className="Enroll-btn">Enroll Now</div> 
- </div>
-</div>
-  )
+        <div className="Service-content">
+          <h2>Foundation in Mobile Repair</h2>
+          <p>
+            Learn Smartphone hardware fundamentals,repair tools, and core
+            techniques from the groud up.
+          </p>
+        </div>
+        <div className="course-details">
+          <div className="detail">
+            <span>
+              <div>Duration</div>
+              <div>Eligibilty</div>
+              <div>Training</div>
+              <div>Certificate</div>
+            </span>
+            <strong>
+              <div>4 weeks</div>
+              <div>10th pass</div>
+              <div>Practical</div>
+              <div>Included</div>
+            </strong>
+          </div>
+        </div>
+        <div className="Enroll-btn">Enroll Now</div>
+      </div>
+    </div>
+  );
 }
 
 export default Courses;
