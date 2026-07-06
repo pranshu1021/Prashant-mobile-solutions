48c3d82544edc89167a5fd1ef3839456aaf4cd4a
pradeep-tanwar166 <pradeeptanwar166@gmail.com>
2026-07-06 12:19:24 +0530

    56th commit

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -2,58 +2,61 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import React from 'react';
 import './Aboutus.css';
+import { useTranslation } from "react-i18next";
+
 function Aboutus() {
+    const {t}=useTranslation();
     return(
         <>
         <Navbar />
 <div className="aboutus">
     
 <div className="Hero-aboutus">
-<h1>About Prashant Mobile Solutions</h1>
-<p>Empowering the next Generatation of Mobile repair professionals while delivering trusted repair services with quality,presision,and care.</p>
+<h1>{t("about.page_title")}</h1>
+<p>{t("about.hero_paragraph")}</p>
 </div>
 
 <div className="story-aboutus">
-    <h3>Our Story</h3>
-    <p>Prashant Mobile Solutions is dedicated to providing relaible mobile repair services and industry-focused training.</p>
-<p>With Years of practical experience, we help customers restore their devices and equip aspiring techinicians with the skills needed to build sucessfull carreers in mobile repairing.</p>
+    <h3>{t("about.our_story_heading")}</h3>
+    <p>{t("about.our_story_paragraphs.0")}</p>
+<p>{t("about.our_story_paragraphs.1")}</p>
 
 </div>
 <div className="Mission-aboutus">
-<h3>Our Mission</h3>
-<p>To deliver dependable repair solutions and high-quality practical training that empowers individuals with real-world technical skills.</p>
+<h3>{t("about.mission_heading")}</h3>
+<p>{t("about.mission_paragraph")}</p>
 
 </div>
 <div className="Vision-aboutus">
-<h3>Our Vision</h3>
-<p>To become a trusted name in mobile repairing and technical education by creating skilled professionals and providing exceptional customer service.</p>
+<h3>{t("about.vision_heading")}</h3>
+<p>{t("about.vision_paragraph")}</p>
 
 </div>
 <div className="Offer-aboutus">
-    <h3>What We Offer</h3>
+    <h3>{t("about.offer_heading")}</h3>
 <ul>
-<li>Mobile Repair Services</li>
-<li>Chip-level Repairing</li>
-<li>Professional Mobile Reparing Courses</li>
-<li>Practical Hands-on Trainings</li>
+<li>{t("about.offer_list.0")}</li>
+<li>{t("about.offer_list.1")}</li>
+<li>{t("about.offer_list.2")}</li>
+<li>{t("about.offer_list.3")}</li>
 
 </ul>
 </div>
 <div className='Whyus-aboutus'>
-<h3>Why Choose Us?</h3>
+<h3>{t("about.why_heading")}</h3>
 <ul>
-<li>Experienced Trainners</li>
-<li>Practical Learning Environment</li>
-<li>Modern  Reparining Equipments </li>
-<li>Affordable Fees & Services</li>
-<li>Customer-Centric Approach</li>
-<li>Ongoing Student Support</li>
+<li>{t("about.why_list.0")}</li>
+<li>{t("about.why_list.1")}</li>
+<li>{t("about.why_list.2")} </li>
+<li>{t("about.why_list.3")}</li>
+<li>{t("about.why_list.4")}</li>
+<li>{t("about.why_list.5")}</li>
 </ul>
 </div>
 <div className='Journey-aboutus'>
-  <h3>Start Your Journey With Us.</h3>  
+  <h3>{t("about.journey_heading")}</h3>  
     
-    <p>Whether you're looking for expert mobile repair services or want to build a successful career in mobile repairing, Prashant Mobile Solutions is here to guide you every step of the way.</p>
+    <p>{t("about.journey_paragraph")}</p>
 
 </div>
 
